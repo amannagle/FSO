935ea8e05eb5da44a9949277f0be3d5b1e3aeeb9
amannagle <aman.nagle1999@gmail.com>
2023-06-22 21:12:53 +0530

    2.8 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,44 +1,48 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
- const handlechange=(e)=>{
-  console.log(e.target.value)
-  setNewName(e.target.value)
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+  const handlechange = (e) => {
+    console.log(e.target.value);
+    setNewName(e.target.value);
+  };
 
- }
-
- const handleSubmit = (e)=>{
-  e.preventDefault();
-  console.log("submit clicked")
-  const newPerson = {
-    name:newName
-  }
-  console.log("person object",newPerson)
-  setPersons([...persons,newPerson])
-  setNewName("")
- }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submit clicked");
+    const names = persons.map((person) => person.name);
+    if (names.includes(newName.toLowerCase())) {
+      alert(`${newName} is already added to the phonebook`);
+      setNewName("");
+      return;
+    }
+    const newPerson = {
+      name: newName,
+    };
+    console.log("person object", newPerson);
+    setPersons([...persons, newPerson]);
+    setNewName("");
+  };
   return (
     <div>
       <h2>Phonebook</h2>
-      <form  onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit}>
         <div>
           name: <input value={newName} onChange={handlechange} />
         </div>
         <div>
-          <button
-          type="submit">add</button>
+          <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <div>
-      {persons.map(person=><p key={person.name}>{person.name}</p>)}
+        {persons.map((person) => (
+          <p key={person.name}>{person.name}</p>
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
